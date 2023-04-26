c8b237c84fd87968a5bbb40344b8bb8f288476fd
yujinpan <1192878390@qq.com>
2023-04-26 15:18:56 +0800

    chore: add vitepress to do docs write

---
+++ .vitepress/config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  appearance: true,
+  base: '/node-base/',
+  title: 'node-base',
+  description: 'infinite scroll for el-table.',
+
+  themeConfig: {
+    logo: '/logo.svg',
+    nav: [{ text: 'Guide', link: '/' }],
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/yujinpan/node-base',
+      },
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-2023 yujinpan',
+    },
+    lastUpdatedText: 'Last Updated',
+  },
+
+  srcExclude: ['!README.md'],
+
+  async transformHtml(code) {
+    return code.replace(
+      '</body>',
+      `
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-xxx"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-xxx');
+</script>
+</body>
+`,
+    );
+  },
+});
